3c7a7b24fbf8f0ceaf13c287e34f74597af1947f
IvanEmanuelCamperchioli <ivanemanuel2137@gmail.com>
2020-08-04 14:55:40 -0300

    creo url de la pagina

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+if (document.querySelector("#perrosGatos")) {
+    
+    const perro1 = document.querySelector("#perro1")
+    const perro2 = document.querySelector("#perro2")
+    const perro3 = document.querySelector("#perro3")
+
+    
+}
